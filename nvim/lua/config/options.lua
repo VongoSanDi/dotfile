98dcc3aa285bd0ee7d42dd85591a6cb0eed11e26
vongo <vongo@andres-debian.home>
2025-03-16 18:45:11 +0100

    add nvim options
    add background dark and turn off swap files

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,8 +4,10 @@
 
 local opt = vim.opt
 
+opt.background = "dark"
 opt.spelllang = { "en_us", "fr" }
 opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
 opt.wrap = true -- Enable line wrapping
 opt.breakindent = true -- Indentation visuelle des lignes repliées
 opt.scrolloff = 5 -- Lines of context
+opt.swapfile = false -- turn off swapfile
