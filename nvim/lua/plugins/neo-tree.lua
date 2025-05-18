8e6bfefa1f84c9cfbf6195eb364680cf23b87fdd
Andres <andresboulanger94@gmail.com>
2025-05-18 22:57:39 +0200

    fix hyprland workspace 3 & open neotree on current window

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,7 +2,8 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
-      width = 30,
+      -- width = 30,
+      position = "current", -- ouvre dans la fenêtre actuelle au lieu de la sidebar
     },
   },
 }
