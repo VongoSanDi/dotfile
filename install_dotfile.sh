ad7ca0b2f7c72df72940a53d7a6fdae811fb2083
Andres <andresboulanger94@gmail.com>
2026-01-21 00:16:00 +1300

    delete rofi

--- install_dotfile.sh
+++ install_dotfile.sh
@@ -8,7 +8,6 @@ echo "🚀 Lancement de l'installation des dotfiles..."
 "$HOME/.dotfile/nvim/install.sh" "$@"
 "$HOME/.dotfile/kitty/install.sh" "$@"
 "$HOME/.dotfile/hypr/install.sh" "$@"
-"$HOME/.dotfile/rofi/install.sh" "$@"
 "$HOME/.dotfile/yazi/install.sh" "$@"
 "$HOME/.dotfile/dnscrypt-proxy/install.sh" "$@"
 
